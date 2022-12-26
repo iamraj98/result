dad906a8dc806667f5c299634b0a4160804691c3
Raj <rajlohar248@gmail.com>
2022-12-26 12:21:05 +0530

    update

---
+++ js/validation.js
@@ -0,0 +1,24 @@
+function validation(){
+  let u_name = document.getElementById("username").value;
+  let u_password = document.getElementById("password").value;
+  if(u_name == '' ){
+    alert("unable to login as there is no user name");
+
+  }
+  else if(u_password == ''){
+    alert("unable to login as there is no password");
+    
+  }
+  else if (u_name == 'admin'){
+    if(u_password == 'admin@1998'){
+      window.open('./info.html','_self');
+    }
+    else{
+      alert("Password Does not match");
+    }
+  }
+  else{
+    alert("user name does not match");
+  }
+  
+}
